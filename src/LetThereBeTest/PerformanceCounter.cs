00d5265ea63c8ac34e88427245d19f9a4a6e3680
Arialdo Martini <arialdomartini@gmail.com>
2013-01-21 10:46:44 +0100

    PerformanceCounter can measure the time elapsed to perform an operation

---
+++ src/LetThereBeTest/PerformanceCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LetThereBeTest
+{
+	public class PerformanceCounter : IDisposable
+	{
+		private readonly Stopwatch _stopwatch;
+		private readonly string _message;
+		private static Action<string, TimeSpan> _action;
+
+		public static IDisposable Start(string message)
+		{
+			return new PerformanceCounter(message, (msg, elapsed) => Console.WriteLine("{0} took {1}", msg, elapsed));
+		}
+
+		public static IDisposable Start(string message, Action<string, TimeSpan> action)
+		{
+			return new PerformanceCounter(message, action);
+		}
+
+		private PerformanceCounter(string message, Action<string, TimeSpan> action)
+		{
+			_stopwatch = new Stopwatch();
+			_message = message;
+			_action = action;
+			_stopwatch.Start();
+		}
+
+		public void Dispose()
+		{
+			_stopwatch.Stop();
+			_action(_message, _stopwatch.Elapsed);
+		}
+	}
+}
